740ed4eb854f0b7723bc8b6a13b57949e1f043fd
SeoyeonPark1223 <trispark1223@gmail.com>
2025-01-17 16:06:15 +0900

    #22 fix: collection/search DTO에 schema 추가

--- src/main/java/com/memesphere/dto/response/CollectionResponseDTO.java
+++ src/main/java/com/memesphere/dto/response/CollectionResponseDTO.java
@@ -1,5 +1,6 @@
 package com.memesphere.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,7 @@ public class CollectionResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CollectionPageDTO {
+        @Schema(description = "콜렉션 아이템들")
         List<CollectionPreviewDTO> collectionItems;
         Integer listSize;
         Integer totalPage;
@@ -27,12 +29,19 @@ public class CollectionResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CollectionPreviewDTO {
+        @Schema(description = "밈코인 name")
         String name;
+        @Schema(description = "밈코인 symbol")
         String symbol;
+        @Schema(description = "밈코인 image")
         String image;
+        @Schema(description = "차트 데이터의 price")
         BigDecimal currentPrice;
+        @Schema(description = "차트 데이터의 high_price")
         BigDecimal highPrice;
+        @Schema(description = "차트 데이터의 low_price")
         BigDecimal lowPrice;
+        @Schema(description = "차트 데이터의 price_change")
         BigDecimal variation;
     }
 }
--- src/main/java/com/memesphere/dto/response/SearchResponseDTO.java
+++ src/main/java/com/memesphere/dto/response/SearchResponseDTO.java
@@ -1,5 +1,6 @@
 package com.memesphere.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +15,9 @@ public class SearchResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SearchPageDTO {
+        @Schema(description = "gridView용 검색 아이템들")
         List<SearchGridPreviewDTO> gridItems; // Grid View용 데이터
+        @Schema(description = "listView용 검색 아이템들")
         List<SearchListPreviewDTO> listItems; // List View용 데이터
         Integer listSize;
         Integer totalPage;
@@ -28,13 +31,21 @@ public class SearchResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SearchGridPreviewDTO {
+        @Schema(description = "밈코인 name")
         String name;
+        @Schema(description = "밈코인 symbol")
         String symbol;
+        @Schema(description = "밈코인 image")
         String image;
+        @Schema(description = "차트 데이터의 price")
         BigDecimal currentPrice;
+        @Schema(description = "차트 데이터의 high_price")
         BigDecimal highPrice;
+        @Schema(description = "차트 데이터의 low_price")
         BigDecimal lowPrice;
+        @Schema(description = "차트 데이터의 price_change")
         BigDecimal variation;
+        @Schema(description = "collection에 해당 밈코인 유무")
         Boolean isCollected;
     }
 
@@ -43,13 +54,20 @@ public class SearchResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SearchListPreviewDTO {
+        @Schema(description = "밈코인 name")
         String name;
+        @Schema(description = "밈코인 symbol")
         String symbol;
         // market cap = currentPrice x volume
+        @Schema(description = "차트 데이터의 price")
         BigDecimal currentPrice;
+        @Schema(description = "차트 데이터의 marketCap")
         BigDecimal market_cap;
+        @Schema(description = "차트 데이터의 volume")
         Integer volume;
+        @Schema(description = "차트 데이터의 price_change")
         BigDecimal variation;
+        @Schema(description = "collection에 해당 밈코인 유무")
         Boolean isCollected;
     }
 }
