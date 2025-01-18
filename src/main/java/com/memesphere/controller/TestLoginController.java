df8132d9e4f82221e92e608c1b427c67bb91fe2e
안혜연 <ahy020606@naver.com>
2025-01-19 05:06:02 +0900

    #8 Chore: 테스트 로그인 관련 코드 수정

--- src/main/java/com/memesphere/controller/TestLoginController.java
+++ src/main/java/com/memesphere/controller/TestLoginController.java
@@ -1,12 +1,19 @@
 package com.memesphere.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class TestLoginController {
 
+    @Value("${testLoginUrl}")
+    private String testLoginUrl;
+
     @GetMapping("/login")
-    public String login() {
-        return "login.html";
+    public String login(Model model) {
+
+        model.addAttribute("testLoginUrl", testLoginUrl);
+        return "login";
     }
 }
