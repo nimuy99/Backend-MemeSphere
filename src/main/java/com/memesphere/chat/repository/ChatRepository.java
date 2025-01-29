aeb47f7adc921baeae995b4003903a835024b1d6
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2025-01-29 20:16:45 +0900

    #35 Chore: 코드 삭제

--- src/main/java/com/memesphere/chat/repository/ChatRepository.java
+++ src/main/java/com/memesphere/chat/repository/ChatRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRepository extends JpaRepository<Chat, Long>, ChatCustomRepository {
 
-    //Chat findFirstByCoinIdOrderByCreatedAtDesc(Long coin_id);
     Chat findFirstByMemeCoin_IdOrderByCreatedAtDesc(Long coin_id);
 }
