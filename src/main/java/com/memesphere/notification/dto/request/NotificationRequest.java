328cb73074771de7c593200bf8c78f64346e9d04
Jiyun <kjy5952@naver.com>
2025-01-30 22:17:48 +0900

    #11 Feat: add schema in notification dto

--- src/main/java/com/memesphere/notification/dto/request/NotificationRequest.java
+++ src/main/java/com/memesphere/notification/dto/request/NotificationRequest.java
@@ -1,5 +1,6 @@
 package com.memesphere.notification.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +8,18 @@ import lombok.Getter;
 @Builder
 public class NotificationRequest {
 
+    @Schema(description = "코인 이름", example = "DOGE")
     private String name;
+
+    @Schema(description = "코인 심볼", example = "DOGE/USDT")
     private String symbol;
+
+    @Schema(description = "변동성", example = "5")
     private Integer volatility;
+
+    @Schema(description = "기준 시간", example = "10")
     private Integer stTime;
+
+    @Schema(description = "상승 또는 하락", example = "True")
     private Boolean isRising;
 }
--- src/main/java/com/memesphere/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/memesphere/notification/dto/response/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.memesphere.notification.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,11 +8,24 @@ import lombok.Getter;
 @Builder
 public class NotificationResponse {
 
+    @Schema(description = "알림 아이디", example = "1")
     private Long notificationId;
+
+    @Schema(description = "코인 이름", example = "DOGE")
     private String name;
+
+    @Schema(description = "코인 심볼", example = "DOGE/USDT")
     private String symbol;
+
+    @Schema(description = "변동성", example = "5")
     private Integer volatility;
+
+    @Schema(description = "기준 시간", example = "10")
     private Integer stTime;
+
+    @Schema(description = "상승 또는 하락", example = "True")
     private Boolean isRising;
+
+    @Schema(description = "알림 켜기/끄기", example = "True")
     private Boolean isOn;
 }
