e8e08ec5c6dcfe2a09d6056a27d1cfc08dfdc1c4
안혜연 <ahy020606@naver.com>
2025-02-11 23:07:19 +0900

    #88 Chore: 코드 정리

--- src/main/java/com/memesphere/domain/user/controller/UserController.java
+++ src/main/java/com/memesphere/domain/user/controller/UserController.java
@@ -21,7 +21,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
