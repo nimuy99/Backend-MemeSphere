d227f8d872dc1c8cc976a2b17ac5c60f2f638e50
nimuy99 <nimuy99@gmail.com>
2025-02-13 20:58:39 +0900

    #105 Feat: 코인 디테일 가격 정보 조회 기능 구현

--- src/main/java/com/memesphere/domain/detail/converter/DetailConverter.java
+++ src/main/java/com/memesphere/domain/detail/converter/DetailConverter.java
@@ -1,7 +1,11 @@
 package com.memesphere.domain.detail.converter;
 
+import com.memesphere.domain.chartdata.entity.ChartData;
+import com.memesphere.domain.detail.dto.response.PriceInfoResponse;
 import com.memesphere.domain.memecoin.entity.MemeCoin;
 import com.memesphere.domain.detail.dto.response.DetailGetResponse;
+
+import java.math.BigDecimal;
 
 public class DetailConverter {
 
@@ -15,6 +19,20 @@ public class DetailConverter {
                 .image(memeCoin.getImage())
                 .keywords(memeCoin.getKeywords())
                 .collectionActive(memeCoin.getCollectionList().isEmpty())
+                .build();
+    }
+
+    public static PriceInfoResponse toPriceInfoResponse(MemeCoin memeCoin, ChartData data) {
+        return PriceInfoResponse.builder()
+                .coinId(memeCoin.getId())
+                .price(data.getPrice())
+                .priceChange(data.getPriceChange())
+                .priceChangeAbsolute(data.getPriceChange().abs())
+                .priceChangeDirection(data.getPriceChangeRate().compareTo(BigDecimal.ZERO) < 0 ? "down" : "up")
+                .priceChangeRate(data.getPriceChangeRate())
+                .weightedAveragePrice(data.getWeighted_average_price())
+                .highPrice(data.getHigh_price())
+                .lowPrice(data.getLow_price())
                 .build();
     }
 }
