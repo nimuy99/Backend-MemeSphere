06f0412c43bc51b5dcb692d5c990f625ba310c12
SeoyeonPark1223 <trispark1223@gmail.com>
2025-02-17 17:32:15 +0900

    #117 Feature: 컬렉션 sortType, viewType 기준 구현

--- src/main/java/com/memesphere/domain/collection/dto/response/CollectionPageResponse.java
+++ src/main/java/com/memesphere/domain/collection/dto/response/CollectionPageResponse.java
@@ -1,5 +1,8 @@
 package com.memesphere.domain.collection.dto.response;
 
+import com.memesphere.domain.collection.entity.Collection;
+import com.memesphere.domain.search.dto.response.SearchGridPreviewResponse;
+import com.memesphere.domain.search.dto.response.SearchListPreviewResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +16,10 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CollectionPageResponse {
-    @Schema(description = "콜렉션 아이템들")
-    List<CollectionPreviewResponse> collectionItems;
+    @Schema(description = "gridView용 컬렉션 아이템들")
+    List<CollectionGridPreviewResponse> gridItems; // Grid View용 데이터
+    @Schema(description = "listView용 컬렉션 아이템들")
+    List<CollectionListPreviewResponse> listItems; // List View용 데이터
     Integer listSize;
     Integer totalPage;
     Long totalElements;
--- src/main/java/com/memesphere/domain/collection/service/CollectionQueryService.java
+++ src/main/java/com/memesphere/domain/collection/service/CollectionQueryService.java
@@ -1,11 +1,13 @@
 package com.memesphere.domain.collection.service;
 
 import com.memesphere.domain.collection.entity.Collection;
+import com.memesphere.domain.search.entity.SortType;
+import com.memesphere.domain.search.entity.ViewType;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface CollectionQueryService {
-    Page<Collection> getCollectionPage(Long userId, Integer pageNumber);
+    Page<Collection> getCollectionPage(Long userId, Integer pageNumber, ViewType viewType, SortType sortType);
     List<Long> getUserCollectionIds(Long userId);
 }
